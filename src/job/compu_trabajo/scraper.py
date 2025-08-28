9dd249cc16808cef2090ee633acbb2f48db77ad8
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-08-27 23:48:57 -0500

    Add compu_trabajo directory to job module for new feature development

---
+++ src/job/compu_trabajo/scraper.py
@@ -0,0 +1,427 @@
+import asyncio
+import json
+import random
+from dataclasses import dataclass
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+from crawl4ai import AsyncWebCrawler, CacheMode, CrawlerRunConfig
+from crawl4ai.extraction_strategy import JsonCssExtractionStrategy
+from crawl4ai.models import CrawlResultContainer
+
+from src.config import DATA_PATH, browser_config
+
+BASE_URL = "https://mx.computrabajo.com"
+ERROR_URL = "https://mx.computrabajo.com/trabajo-de-python?p=95"
+JOB_URL = f"{BASE_URL}/trabajo-de-python"
+KEY_CSS_SELECTOR = "#offersGridOfferContainer"
+DETAIL_CSS_SELECTOR = "div.box_detail"
+DETAIL_ICONS = {
+    "i_clock": "time",
+    "i_find": "location",
+    "i_company": "place",
+    "i_money": "salary",
+    "i_home": "place",
+}
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+output_schema = {
+    "name": "Computrabajo Job Scraper",
+    "baseSelector": "article.box_offer",
+    "fields": [
+        {"name": "title", "selector": "a.js-o-link", "type": "text"},
+        {"name": "company", "selector": "p.dFlex", "type": "text"},
+        {
+            "name": "location",
+            "selector": "p:nth-child(3)",
+            "type": "text",
+        },
+        {
+            "name": "relative_date",
+            "selector": "p.fs13.fc_aux.mt15",
+            "type": "text",
+        },
+        {
+            "name": "description",
+            "selector": "div.fs16.t_word_wrap",
+            "type": "text",
+        },
+    ],
+}
+
+
+def get_job_details(soup: BeautifulSoup) -> dict:
+    """
+    Extract job details from the job description page.
+    """
+    box_detail = soup.find("div", class_="box_detail")
+    fs14_div = box_detail.find("div", class_="fs14")
+    job_url = None
+
+    if not fs14_div:
+        return {}
+
+    if bubble := box_detail.find("div", class_="opt_bubble"):
+        job_url = f"{BASE_URL}{bubble.attrs['data-url']}"
+
+    all_ps = fs14_div.find_all("p")
+
+    description = " ".join(
+        box_detail.find("div", class_="t_word_wrap").text.strip().split("\n")
+    )
+    requirements = box_detail.find("ul", class_="disc").text.strip()
+
+    dict_data = {
+        "description": description,
+        "requirements": requirements,
+        "job_url": job_url,
+    }
+
+    for p in all_ps:
+        span_class = "__".join(p.find("span").attrs.get("class"))
+        text = p.text.strip()
+
+        for icon_class, key in DETAIL_ICONS.items():
+            if icon_class in span_class:
+                dict_data[key] = text
+                break
+
+    return dict_data
+
+
+BASE_SELECTOR = "article.box_offer"
+
+
+class MainPageSetup:
+
+    def __init__(self):
+        self._base_selector = "article.box_offer"
+
+    @property
+    def session_id(self) -> str:
+        """
+        Generate a unique session ID for the scraper.
+        """
+        return None
+
+    @property
+    def service_name(self) -> str:
+        return "compu_trabajo"
+
+    def _output_schema(self):
+        return {
+            "name": self.service_name + " Job Scraper",
+            "baseSelector": BASE_SELECTOR,
+            "fields": [
+                {"name": "title", "selector": "a.js-o-link", "type": "text"},
+                {"name": "company", "selector": "p.dFlex", "type": "text"},
+                {
+                    "name": "location",
+                    "selector": "p:nth-child(3)",
+                    "type": "text",
+                },
+                {
+                    "name": "relative_date",
+                    "selector": "p.fs13.fc_aux.mt15",
+                    "type": "text",
+                },
+                {
+                    "name": "description",
+                    "selector": "div.fs16.t_word_wrap",
+                    "type": "text",
+                },
+            ],
+        }
+
+    def _get_crawler_config(self):
+        strategy = JsonCssExtractionStrategy(self._output_schema())
+
+        return CrawlerRunConfig(
+            extraction_strategy=strategy,
+            wait_for=KEY_CSS_SELECTOR,
+            session_id=self.session_id,
+            wait_for_timeout=2_000,
+        )
+
+
+@dataclass
+class Scraper(MainPageSetup):
+    url: str
+    crawler: AsyncWebCrawler
+
+    @property
+    def session_id(self) -> str:
+        return f"{self.service_name}_scraper"
+
+    def set_url(self, url: str) -> None:
+        self.url = url
+
+    def _get_offer_id(self, soup: BeautifulSoup) -> str | None:
+        """
+        Extract the job offer ID from the URL.
+        """
+        return soup.find(id="IdOffer").attrs.get("value")
+
+    async def _get_overview(self) -> dict | None:
+        result = await self.crawler.arun(
+            url=self.url,
+            config=self._get_crawler_config(),
+        )
+
+        if not result.success:
+            print("Error:", result.error_message)
+            return False
+
+        return json.loads(result.extracted_content)
+
+    async def _get_details(self, idx: int, offer: dict) -> dict:
+        """
+        Retrieve details from the page
+        """
+        js = f"document.querySelectorAll('article.box_offer')[{idx}].click();"
+
+        config_click = CrawlerRunConfig(
+            js_code=js,
+            js_only=True,
+            wait_for=DETAIL_CSS_SELECTOR,
+            session_id=self.session_id,
+            wait_for_timeout=5_000,
+        )
+        result_detail = await self.crawler.arun(url=self.url, config=config_click)
+
+        if result_detail.success:
+            # We use bs4 because the complex structure of the job details
+            soup = BeautifulSoup(result_detail.html, "html.parser")
+            offer["details"] = get_job_details(soup)
+            offer["offer_id"] = self._get_offer_id(soup)
+            offer["current_datetime"] = datetime.now().strftime(DATE_FORMAT)
+
+        return offer
+
+    async def click_next_page(self) -> CrawlResultContainer:
+        """
+        Click the next page button
+        """
+        js_next = "document.querySelector('span.b_primary.w48.buildLink.cp').click();"
+
+        config_click_next = CrawlerRunConfig(
+            js_code=js_next,
+            js_only=True,
+            wait_for=DETAIL_CSS_SELECTOR,
+            session_id=self.session_id,
+            wait_for_timeout=5_000,
+        )
+        return await self.crawler.arun(url=self.url, config=config_click_next)
+
+    @property
+    async def is_next_page_available(self) -> bool:
+        """
+        Check if the next page is available.
+        """
+        config_click_next = CrawlerRunConfig(session_id=self.session_id)
+
+        result = await self.crawler.arun(url=self.url, config=config_click_next)
+
+        soup = BeautifulSoup(result.html, "html.parser")
+        next_page_button = soup.select_one(DETAIL_CSS_SELECTOR)
+        return bool(next_page_button)
+
+    async def get_data(self):
+        """
+        Retrieve job data from the website.
+        """
+
+        if not (offers := await self._get_overview()):
+            return None
+
+        for idx, offer in enumerate(offers):
+            try:
+                offers[idx] = await self._get_details(idx, offer)
+            except Exception as e:
+                print(f"Error retrieving details for offer {idx}: {e}")
+
+        return offers
+
+
+async def main_scraper():
+    async with AsyncWebCrawler(config=browser_config) as crawler:
+        # offers_available = True
+        scraper = Scraper(url=JOB_URL, crawler=crawler)
+        # Initial chunk
+        all_offers = await scraper.get_data()
+
+        for url_idx in range(2, 100):
+            new_url = f"{JOB_URL}?p={url_idx}"
+            scraper.set_url(new_url)
+            if not await scraper.is_next_page_available:
+                break
+            offers = await scraper.get_data()
+            all_offers.extend(offers)
+
+    # Save all offers to a JSON file
+    current_time = datetime.now().strftime("%Y%m%d_%H_00")
+    file_name = DATA_PATH / f"{scraper.service_name}_job_offers_{current_time}.json"
+
+    with open(file_name, "w") as f:
+        json.dump(all_offers, f, indent=4)
+
+
+async def get_details(
+    crawler: AsyncWebCrawler, url: str, session_id: str, idx: int, offer: dict
+) -> dict:
+    """
+    Retrieve details from the page
+    """
+    js = f"document.querySelectorAll('article.box_offer')[{idx}].click();"
+
+    config_click = CrawlerRunConfig(
+        js_code=js,
+        js_only=True,
+        wait_for=DETAIL_CSS_SELECTOR,
+        session_id=session_id,
+        wait_for_timeout=5_000,
+    )
+    result_detail = await crawler.arun(url=url, config=config_click)
+
+    if result_detail.success:
+        # We use bs4 because the complex structure of the job details
+        soup = BeautifulSoup(result_detail.html, "html.parser")
+        offer["details"] = get_job_details(soup)
+        # offer["offer_id"] = self._get_offer_id(soup)
+        offer["current_datetime"] = datetime.now().strftime(DATE_FORMAT)
+
+    return offer
+
+
+async def parallel_scraping():
+
+    strategy = JsonCssExtractionStrategy(
+        {
+            "name": "Computrabajo Job Scraper",
+            "baseSelector": BASE_SELECTOR,
+            "fields": [
+                {"name": "title", "selector": "a.js-o-link", "type": "text"},
+                {"name": "company", "selector": "p.dFlex", "type": "text"},
+                {
+                    "name": "location",
+                    "selector": "p:nth-child(3)",
+                    "type": "text",
+                },
+                {
+                    "name": "relative_date",
+                    "selector": "p.fs13.fc_aux.mt15",
+                    "type": "text",
+                },
+                {
+                    "name": "description",
+                    "selector": "div.fs16.t_word_wrap",
+                    "type": "text",
+                },
+            ],
+        }
+    )
+
+    config = CrawlerRunConfig(
+        extraction_strategy=strategy,
+        wait_for=KEY_CSS_SELECTOR,
+        # session_id=self.session_id,
+        wait_for_timeout=2_000,
+        stream=True,
+        # simulate_user=True,
+        # mean_delay=1.5,
+        # delay_before_return_html=1.0,
+    )
+    all_data = []
+    async with AsyncWebCrawler(config=browser_config) as crawler:
+        all_urls = [JOB_URL] + [f"{JOB_URL}?p={i}" for i in range(2, 100)]
+        id_counter = 0
+
+        async for result in await crawler.arun_many(urls=all_urls, config=config):
+            id_counter += 1
+            session_id = f"job_listing_{id_counter}"
+            print(f"{result=}")
+            if not result.success:
+                print("Error:", result.error_message)
+                continue
+            offers = json.loads(result.extracted_content)
+
+            for idx, offer in enumerate(offers):
+                try:
+                    offers[idx] = await get_details(
+                        crawler, result.url, session_id, idx, offer
+                    )
+                except Exception as e:
+                    print(f"Error retrieving details for offer {idx}: {e}")
+
+            # all_data.append(offers)
+
+        return all_data
+
+
+async def main():
+    # random number
+    random_number = random.randint(1000, 9999)
+    SESSION_ID = f"job_listings_session_{random_number}"
+
+    strategy = JsonCssExtractionStrategy(output_schema)
+
+    crawler_config = CrawlerRunConfig(
+        extraction_strategy=strategy,
+        wait_for=KEY_CSS_SELECTOR,
+        session_id=SESSION_ID,  # Keep session for job listings
+        wait_for_timeout=5_000,
+    )
+
+    # Create an instance of AsyncWebCrawler
+    async with AsyncWebCrawler(config=browser_config) as crawler:
+        # Run the crawler on a URL
+        result = await crawler.arun(url=ERROR_URL, config=crawler_config)
+
+        if not result.success:
+            print("Error:", result.error_message)
+            return
+
+        # Save the result to a JSON file
+        offers = json.loads(result.extracted_content)
+
+        for idx, _ in enumerate(offers):
+            js = f"document.querySelectorAll('article.box_offer')[{idx}].click();"
+
+            config_click = CrawlerRunConfig(
+                js_code=js,
+                wait_for=DETAIL_CSS_SELECTOR,
+                session_id=SESSION_ID,
+                cache_mode=CacheMode.ENABLED,
+                wait_for_timeout=5_000,
+            )
+            result_detail = await crawler.arun(url=JOB_URL, config=config_click)
+
+            if result_detail.success:
+                # We use bs4 because the complex structure of the job details
+                soup = BeautifulSoup(result_detail.html, "html.parser")
+                dict_data = get_job_details(soup)
+                offers[idx]["details"] = dict_data
+
+        # Click next page
+        js_next = [
+            "console.log('dummy')",
+            "document.querySelector('span.b_primary.w48.buildLink.cp').click();",
+        ]
+
+        config_click_next = CrawlerRunConfig(
+            js_code=js_next,
+            js_only=True,
+            wait_for=DETAIL_CSS_SELECTOR,
+            session_id=SESSION_ID,
+            wait_for_timeout=5_000,
+        )
+        result_detail = await crawler.arun(url=JOB_URL, config=config_click_next)
+        result_detail = await crawler.arun(url=JOB_URL, config=config_click_next)
+
+        print(offers)
+
+
+if __name__ == "__main__":
+    # Run the async main function
+    asyncio.run(main_scraper())
+    # asyncio.run(parallel_scraping())
---
+++ src/job/compu_trabajo/utils.py
@@ -0,0 +1,85 @@
+from datetime import datetime, timedelta
+
+MONTHS = {
+    "enero": 1,
+    "febrero": 2,
+    "marzo": 3,
+    "abril": 4,
+    "mayo": 5,
+    "junio": 6,
+    "julio": 7,
+    "agosto": 8,
+    "septiembre": 9,
+    "octubre": 10,
+    "noviembre": 11,
+    "diciembre": 12,
+}
+
+DEFAULT_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+class DateConverter:
+
+    def __init__(self, raw_date: str) -> None:
+        self.raw_date = raw_date.lower()
+        self._digit = self._get_digit()
+        self._current_date = datetime.now()
+
+    def _get_digit(self) -> str:
+        return "".join([el for el in list(self.raw_date) if el.isdigit()])
+
+    @property
+    def is_yesterday(self) -> bool:
+        return "ayer" in self.raw_date
+
+    @property
+    def is_hour(self) -> bool:
+        return "horas" in self.raw_date or "hora" in self.raw_date
+
+    @property
+    def is_minutes(self) -> bool:
+        return "minutos" in self.raw_date or "minuto" in self.raw_date
+
+    @property
+    def is_days(self) -> bool:
+        return "días" in self.raw_date
+
+    def _convert_from_minutes(self) -> datetime:
+        return self._current_date - timedelta(minutes=int(self._digit))
+
+    def _convert_from_hour(self) -> datetime:
+        return self._current_date - timedelta(hours=int(self._digit))
+
+    def _convert_from_yesterday(self) -> datetime:
+        return self._current_date - timedelta(days=1)
+
+    def _convert_from_days(self) -> datetime:
+        return self._current_date - timedelta(days=int(self._digit))
+
+    def _default(self) -> datetime:
+        month_name = [el for el in self.raw_date.split() if el in MONTHS][0]
+
+        if not month_name:
+            raise ValueError(f"Month not found in {self.raw_date}")
+
+        month_number = MONTHS.get(month_name)
+        day = int(self._digit)
+        return datetime(self._current_date.year, month_number, day)
+
+    def convert(self) -> str:
+        """
+        Convert a relative date string to an absolute date string.
+        """
+        if self.is_yesterday:
+            standard_date = self._convert_from_yesterday()
+        elif self.is_hour:
+            standard_date = self._convert_from_hour()
+        elif self.is_days:
+            standard_date = self._convert_from_days()
+        elif self.is_minutes:
+            standard_date = self._convert_from_minutes()
+        else:
+            print(self.raw_date)
+            standard_date = self._default()
+
+        return standard_date.strftime(DEFAULT_FORMAT)
